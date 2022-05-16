22e632502adcf5eb25ce61ec8d28962dafae1295
Cliel <samuel.sicard.it@gmail.com>
2022-05-16 22:21:30 +0200

    Init project on desktop

---
+++ src/main/java/com/seihitsu/seihitsuback/SeihitsuBackApplication.java
@@ -0,0 +1,14 @@
+package com.seihitsu.seihitsuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeihitsuBackApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(SeihitsuBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seihitsu/seihitsuback/SeihitsuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seihitsu.seihitsuback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeihitsuBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
