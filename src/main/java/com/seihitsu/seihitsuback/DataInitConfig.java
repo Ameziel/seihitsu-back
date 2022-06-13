ce9303ffecf9965ac80b983e116e635b64dc01e0
Cliel <samuel.sicard.it@gmail.com>
2022-06-13 22:53:15 +0200

    init data

---
+++ src/main/java/com/seihitsu/seihitsuback/DataInitConfig.java
@@ -0,0 +1,51 @@
+package com.seihitsu.seihitsuback;
+
+import com.seihitsu.seihitsuback.client.Client;
+import com.seihitsu.seihitsuback.client.ClientRepository;
+import com.seihitsu.seihitsuback.employe.Employe;
+import com.seihitsu.seihitsuback.employe.EmployeRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+/**
+ * Valeur de départ pour l'application
+ * @author Ameziel
+ */
+@Configuration
+public class DataInitConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(ClientRepository clientRepository, EmployeRepository employeRepository) {
+        return args -> {
+            Client a = new Client(
+                    "sam",
+                    "sic",
+                    "email@gmail.com"
+            );
+            Client b = new Client(
+                    "sam",
+                    "sic",
+                    "email@gmail.com"
+            );
+            clientRepository.saveAll
+                    (List.of(a,b)
+                    );
+            Employe employe1 = new Employe(
+                    "sam",
+                    "sic",
+                    "Maitre d'hotel"
+            );
+            Employe employe2 = new Employe(
+                    "Samantha",
+                    "sic",
+                    "Hote de caisse"
+            );
+            employeRepository.saveAll
+                    (List.of(employe1,employe2)
+                    );
+
+        };
+    }
+}
