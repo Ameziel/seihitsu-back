e2cbd0ec24606e1851f89f22ea41206ce6b0ed2f
Cliel <samuel.sicard.it@gmail.com>
2022-06-14 13:10:18 +0200

    Chambre class update. (Constructor + mapping)

--- src/main/java/com/seihitsu/seihitsuback/chambre/Chambre.java
+++ src/main/java/com/seihitsu/seihitsuback/chambre/Chambre.java
@@ -1,18 +1,46 @@
 package com.seihitsu.seihitsuback.chambre;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.seihitsu.seihitsuback.sejour.Sejour;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
-
+@Entity
+@Table(name = "chambre")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Chambre {
 
+    @Id
+    @GeneratedValue
     private Long idChambre;
-
+    @Column(name = "prixNuitee")
     private int prixNuitee;
-
+    @Column(name = "categorie")
     private String categorie;
-
+    @Column(name = "description")
     private String description;
+
+    @OneToMany(mappedBy = "chambre")
+    @JsonIgnoreProperties(value = { "chambre" }, allowSetters = true)
+    private Set<Sejour> sejours = new HashSet<>();
+
+    /**
+     * Constructeur chambre
+     * @param prixNuitee
+     * @param categorie
+     * @param description
+     */
+    public Chambre(int prixNuitee, String categorie, String description) {
+        this.prixNuitee = prixNuitee;
+        this.categorie = categorie;
+        this.description = description;
+    }
 }
